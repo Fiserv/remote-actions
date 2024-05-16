579fff289a88858f0f004d4ef7697f53e0b172ef
Minh Pham <f62q1zz>
2024-05-16 14:20:31 -0700

    Prettify code

--- scripts/download-api-zip.js
+++ scripts/download-api-zip.js
@@ -1,213 +1,264 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const yaml = require('js-yaml');
-const SwaggerParser = require('@apidevtools/swagger-parser');
-const AdmZip = require('adm-zip');
-const postmanConverter = require('openapi-to-postmanv2');
+const fs = require("fs");
+const yaml = require("js-yaml");
+const SwaggerParser = require("@apidevtools/swagger-parser");
+const AdmZip = require("adm-zip");
+const postmanConverter = require("openapi-to-postmanv2");
 const args = process.argv.slice(2);
 const folder = args?.[0];
-const { errorMessage, warningMsg , printMessage , provideReferenceFolder} = require('./utils/tools');
-const postman_zip  = new AdmZip() , spec_zip = new AdmZip();
-const specZipFile = args[0]?.includes("/") ? args[0].split('/').pop()+'_spec' : 'tennat_spec';
-const postmanZipFile = args[0]?.includes("/") ? args[0].split('/').pop()+'_postman' : 'tennat_postman';
-const Zip_Generator = 'ZIP GENERATOR';
+const {
+  errorMessage,
+  warningMsg,
+  printMessage,
+  provideReferenceFolder,
+} = require("./utils/tools");
+const postman_zip = new AdmZip(),
+  spec_zip = new AdmZip();
+const specZipFile = args[0]?.includes("/")
+  ? args[0].split("/").pop() + "_spec"
+  : "tennat_spec";
+const postmanZipFile = args[0]?.includes("/")
+  ? args[0].split("/").pop() + "_postman"
+  : "tennat_postman";
+const Zip_Generator = "ZIP GENERATOR";
 
-const validateDir = async (dir) => { 
-
+const validateDir = async (dir) => {
   const files = await fs.promises.readdir(dir, { withFileTypes: true });
-    
-    let check = true;
-    
-    for (const file of files) { 
-      if (file.isDirectory()) { 
-        await validateDir(`${dir}/${file.name}`); 
-      } else{
-        if (file.name === 'tenant.json'){
-          try{ 
-            const fileName = `${dir}/${file.name}`;
-            const content = await fs.promises.readFile(fileName, {encoding: 'UTF8'}); 
-            const data = JSON.parse(content); 
-            check = await validateSpecExistence(args?.[0] , data); 
-          }catch (e){
-            errorMessage(Zip_Generator  ,e?.message);
-            check = false;
-          }  
-          if (check){ 
-            printMessage(`${Zip_Generator} : PASSED`);
-          }else{
-            errorMessage(Zip_Generator , 'FAILED');
-          }
-        }   
-      }  
-    } 
-    return check;
-  };
 
+  let check = true;
 
-    const validateSpecExistence = async(dir , tenantData) => {  
-      let specExistence = true;
-      if (tenantData?.apiVersions && tenantData?.apiVersions.length > 0){
-        for (const item of tenantData.apiVersions) {  
-          const version = item?.version ;   
-          const apiSpecFiles = item.apiSpecFileNames ;  
-          if (apiSpecFiles.length > 0){
-            for (const filePath of apiSpecFiles) { 
-              const file = `${provideReferenceFolder(dir)}/${version}/${filePath}.yaml`;
-              if (!fs.existsSync(file)) {  
-                warningMsg(`${file} - Missing`);
-                specExistence = false;
-              }else{
+  for (const file of files) {
+    if (file.isDirectory()) {
+      await validateDir(`${dir}/${file.name}`);
+    } else {
+      if (file.name === "tenant.json") {
+        try {
+          const fileName = `${dir}/${file.name}`;
+          const content = await fs.promises.readFile(fileName, {
+            encoding: "UTF8",
+          });
+          const data = JSON.parse(content);
+          check = await validateSpecExistence(args?.[0], data);
+        } catch (e) {
+          errorMessage(Zip_Generator, e?.message);
+          check = false;
+        }
+        if (check) {
+          printMessage(`${Zip_Generator} : PASSED`);
+        } else {
+          errorMessage(Zip_Generator, "FAILED");
+        }
+      }
+    }
+  }
+  return check;
+};
 
-                try{  
-                  const content = await fs.promises.readFile(file, 'utf8');
-                  const apiJson = yaml.load(content);
-                  if (!apiJson.paths || !Object.keys(apiJson.paths).length) {
-                  errorMessage(Zip_Generator, 'No path provided!');
-                  continue;
-                }
-                const parsedData = await SwaggerParser.validate(apiJson); 
+const validateSpecExistence = async (dir, tenantData) => {
+  let specExistence = true;
+  if (tenantData?.apiVersions && tenantData?.apiVersions.length > 0) {
+    for (const item of tenantData.apiVersions) {
+      const version = item?.version;
+      const apiSpecFiles = item.apiSpecFileNames;
+      if (apiSpecFiles.length > 0) {
+        for (const filePath of apiSpecFiles) {
+          const file = `${provideReferenceFolder(
+            dir
+          )}/${version}/${filePath}.yaml`;
+          if (!fs.existsSync(file)) {
+            warningMsg(`${file} - Missing`);
+            specExistence = false;
+          } else {
+            try {
+              const content = await fs.promises.readFile(file, "utf8");
+              const apiJson = yaml.load(content);
+              if (!apiJson.paths || !Object.keys(apiJson.paths).length) {
+                errorMessage(Zip_Generator, "No path provided!");
+                continue;
+              }
+              const parsedData = await SwaggerParser.validate(apiJson);
 
-                if (parsedData) {
-                  let check = false;
-                  for (const [path, obj] of Object.entries(apiJson.paths)) {
-                    for (const [reqType, api] of Object.entries(obj)) {
-                      if (typeof api !== 'object' || api === null) { continue; }
-                      if ( api['x-proxy-name']) {
-                        check = true;
-                      } else {
-                        if (!api.hasOwnProperty('x-proxy-name')) {
-                          errorMessage(Zip_Generator, `${fileName} - Missing 'x-proxy-name'`);
-                        }
-                        check = false;
-                        break;
-                      }
+              if (parsedData) {
+                let check = false;
+                for (const [path, obj] of Object.entries(apiJson.paths)) {
+                  for (const [reqType, api] of Object.entries(obj)) {
+                    if (typeof api !== "object" || api === null) {
+                      continue;
                     }
-                    if (!check) {
+                    if (api["x-proxy-name"]) {
+                      check = true;
+                    } else {
+                      if (!api.hasOwnProperty("x-proxy-name")) {
+                        errorMessage(
+                          Zip_Generator,
+                          `${fileName} - Missing 'x-proxy-name'`
+                        );
+                      }
+                      check = false;
                       break;
                     }
                   }
-                  if (check) { 
-                    const tenant_repo = getTenantRepoName(file);
-              
-                    const splitPath= file.split('/'); 
-                    const fileName = splitPath[splitPath.length - 1];
-                    if (fileName !== null){
-                      tenant_repo.path = tenant_repo?.path.replace(`/${fileName}`, '');
-                    } 
-                    if (await generateSpecZipCollection(tenant_repo ,  fileName , content ) 
-                            && tenant_repo !== null 
-                            &&  fileName!== null 
-                            && content !== null){
-                      printMessage(`Spec File : ${fileName} added to Zip`);
-                    }
-            
-                    const postmanFileName = fileName.includes('.yaml') ? fileName.split('.yaml')[0].concat('.json') : `${fileName}.json`;
-                    
-                    if ( tenant_repo !== null   &&  postmanFileName!== null   && content !== null ){
-                        
-                      const check =  generatePostmanCollections(tenant_repo , postmanFileName , content ) ;       
-                        if (check){
-                          printMessage(`Postman Collection: ${fileName} added to Zip`);
-                        }else{
-                          warningMsg(`Unable to create postman Collection for file: ${fileName}`);
-                        } 
+                  if (!check) {
+                    break;
+                  }
+                }
+                if (check) {
+                  const tenant_repo = getTenantRepoName(file);
+
+                  const splitPath = file.split("/");
+                  const fileName = splitPath[splitPath.length - 1];
+                  if (fileName !== null) {
+                    tenant_repo.path = tenant_repo?.path.replace(
+                      `/${fileName}`,
+                      ""
+                    );
+                  }
+                  if (
+                    (await generateSpecZipCollection(
+                      tenant_repo,
+                      fileName,
+                      content
+                    )) &&
+                    tenant_repo !== null &&
+                    fileName !== null &&
+                    content !== null
+                  ) {
+                    printMessage(`Spec File : ${fileName} added to Zip`);
+                  }
+
+                  const postmanFileName = fileName.includes(".yaml")
+                    ? fileName.split(".yaml")[0].concat(".json")
+                    : `${fileName}.json`;
+
+                  if (
+                    tenant_repo !== null &&
+                    postmanFileName !== null &&
+                    content !== null
+                  ) {
+                    const check = generatePostmanCollections(
+                      tenant_repo,
+                      postmanFileName,
+                      content
+                    );
+                    if (check) {
+                      printMessage(
+                        `Postman Collection: ${fileName} added to Zip`
+                      );
+                    } else {
+                      warningMsg(
+                        `Unable to create postman Collection for file: ${fileName}`
+                      );
                     }
                   }
-                }  
-                }catch(e){
-                  warningMsg(`${Zip_Generator} - ${e?.message}`); 
-                } 
-              } 
-            }
-          }   
-        }  
-      }  
-      return specExistence;
-    };
- 
-      const getTenantRepoName = (str) => { 
-          const data = {} 
-          const parts = str.split(str.includes('references') ? '/references' : '/reference');  
-          if (parts.length > 1) {   
-            const arr = parts[0].split('/'); 
-            const name = arr[arr.length - 1];  
-            const filePath = parts[1];
-            data.repo = name;
-            data.path = filePath; 
-            return data;
-          }
-          return null; 
-      };
-
-      const generateSpecZipCollection = async(folder , filename , content ) => {  
-          try{
-    
-            if (folder === '../reference') { 
-              spec_zip.addFile(filename, Buffer.from(content, 'utf8'), 'Adding folders');
-            } else {
-              spec_zip.addFile(`${folder?.repo}/${folder?.path}/${filename}`, Buffer.from(content, 'utf8'), 'Adding file');
-            }
-            spec_zip.writeZip(`${args}/assets/${specZipFile}.zip`);
-          
-          }catch(e){
-            errorMessage('SPEC ZIP GENERATOR', e.message);
-            return false;
-          }
-        return true;
-      };
-
-      const generatePostmanCollections = (folder ,postmanFileName ,content ) => {  
-        let check = false;
-        try{    
-        
-        const timeout = setTimeout(() => {
-          console.log('Postman conversion timed out');
-          return false;
-        }, 5000);
-        
-        postmanConverter.convert(
-          { type: 'string', data: content },
-          {},
-          (err, conversionResult) => {
-            clearTimeout(timeout);
-            if (err !== null) {
-              errorMessage('Postman GENERATOR - Could not convert spec file', err);
-            }
-            if (conversionResult.result) {
-              printMessage(`Adding file postman ----- ${folder?.repo}${folder?.path}/${postmanFileName}`);
-              if (folder === '../reference') {
-                postman_zip.addFile(`${postmanFileName}`, JSON.stringify(conversionResult?.output[0]?.data));
-              } else {
-                postman_zip.addFile(`${folder?.repo}${folder?.path}/${postmanFileName}`, JSON.stringify(conversionResult?.output[0]?.data));
+                }
               }
-              postman_zip.writeZip(`${args}/assets/${postmanZipFile}.zip`);
-              check = true;
-            }  
+            } catch (e) {
+              warningMsg(`${Zip_Generator} - ${e?.message}`);
+            }
           }
-        ); 
-        }catch (error) {
-          errorMessage('Postman GENERATOR', error); 
-          check = false;
-        } 
-        return check;
-      };
-
-
-  
-    const main = async() => { 
-      try {
-        printMessage(`External Dir ---->>> ${args}`);
-        if (args?.length > 0) { 
-        await validateDir(folder+"/config"); 
-        } else {
-          errorMessage(Zip_Generator, 'No Path for reference dir. defined');
         }
-      } catch (e) {
-        errorMessage(Zip_Generator, e.message);
       }
-    };
-
-    if (require.main === module) {
-      main();
     }
+  }
+  return specExistence;
+};
+
+const getTenantRepoName = (str) => {
+  const data = {};
+  const parts = str.split(
+    str.includes("references") ? "/references" : "/reference"
+  );
+  if (parts.length > 1) {
+    const arr = parts[0].split("/");
+    const name = arr[arr.length - 1];
+    const filePath = parts[1];
+    data.repo = name;
+    data.path = filePath;
+    return data;
+  }
+  return null;
+};
+
+const generateSpecZipCollection = async (folder, filename, content) => {
+  try {
+    if (folder === "../reference") {
+      spec_zip.addFile(
+        filename,
+        Buffer.from(content, "utf8"),
+        "Adding folders"
+      );
+    } else {
+      spec_zip.addFile(
+        `${folder?.repo}/${folder?.path}/${filename}`,
+        Buffer.from(content, "utf8"),
+        "Adding file"
+      );
+    }
+    spec_zip.writeZip(`${args}/assets/${specZipFile}.zip`);
+  } catch (e) {
+    errorMessage("SPEC ZIP GENERATOR", e.message);
+    return false;
+  }
+  return true;
+};
+
+const generatePostmanCollections = (folder, postmanFileName, content) => {
+  let check = false;
+  try {
+    const timeout = setTimeout(() => {
+      console.log("Postman conversion timed out");
+      return false;
+    }, 5000);
+
+    postmanConverter.convert(
+      { type: "string", data: content },
+      {},
+      (err, conversionResult) => {
+        clearTimeout(timeout);
+        if (err !== null) {
+          errorMessage("Postman GENERATOR - Could not convert spec file", err);
+        }
+        if (conversionResult.result) {
+          printMessage(
+            `Adding file postman ----- ${folder?.repo}${folder?.path}/${postmanFileName}`
+          );
+          if (folder === "../reference") {
+            postman_zip.addFile(
+              `${postmanFileName}`,
+              JSON.stringify(conversionResult?.output[0]?.data)
+            );
+          } else {
+            postman_zip.addFile(
+              `${folder?.repo}${folder?.path}/${postmanFileName}`,
+              JSON.stringify(conversionResult?.output[0]?.data)
+            );
+          }
+          postman_zip.writeZip(`${args}/assets/${postmanZipFile}.zip`);
+          check = true;
+        }
+      }
+    );
+  } catch (error) {
+    errorMessage("Postman GENERATOR", error);
+    check = false;
+  }
+  return check;
+};
+
+const main = async () => {
+  try {
+    printMessage(`External Dir ---->>> ${args}`);
+    if (args?.length > 0) {
+      await validateDir(folder + "/config");
+    } else {
+      errorMessage(Zip_Generator, "No Path for reference dir. defined");
+    }
+  } catch (e) {
+    errorMessage(Zip_Generator, e.message);
+  }
+};
+
+if (require.main === module) {
+  main();
+}
--- scripts/validator-yaml.js
+++ scripts/validator-yaml.js
@@ -1,90 +1,108 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const yaml = require('js-yaml');
-const SwaggerParser = require('@apidevtools/swagger-parser'); 
-const args = process.argv.slice(2); 
-const folder = args?.[0]; 
-const YAML_VALIDATOR = 'YAML VALIDATOR';
-const showdown = require('showdown');
-const {errorMsg,errorMessage  , printMessage , provideReferenceFolder } = require('./utils/tools');
-const { enrichHTMLFromMarkup, showdownHighlight } = require('./utils/md-utils'); 
-const { exit } = require('process');
- 
-const validateDir = async (dir) => { 
-  
-  let check = false;
-  
-  fs.readdir(dir, { withFileTypes: true }, (err, files) => {
-    files?.forEach(async file => {
+const fs = require("fs");
+const yaml = require("js-yaml");
+const SwaggerParser = require("@apidevtools/swagger-parser");
+const args = process.argv.slice(2);
+const folder = args?.[0];
+const YAML_VALIDATOR = "YAML VALIDATOR";
+const showdown = require("showdown");
+const {
+  errorMsg,
+  errorMessage,
+  printMessage,
+  provideReferenceFolder,
+} = require("./utils/tools");
+const { enrichHTMLFromMarkup, showdownHighlight } = require("./utils/md-utils");
+const { exit } = require("process");
 
+const validateDir = async (dir) => {
+  let check = false;
+
+  fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+    files?.forEach(async (file) => {
       if (file?.isDirectory()) {
         validateDir(`${dir}/${file.name}`);
-      } else if (/\.yaml$/.test(file.name)){
-
+      } else if (/\.yaml$/.test(file.name)) {
         try {
           let fileName = `${dir}/${file.name}`;
-          const content = fs.readFileSync(fileName, 'utf8');
+          const content = fs.readFileSync(fileName, "utf8");
           const apiJson = yaml.load(content);
-          const tenantName =  args[0].split('/').pop(); 
+          const tenantName = args[0].split("/").pop();
           fileName = fileName.split(`/${tenantName}/`)[1];
           if (!apiJson.paths || !Object.keys(apiJson.paths).length) {
-            errorMessage(YAML_VALIDATOR, 'No path provided!');
+            errorMessage(YAML_VALIDATOR, "No path provided!");
           }
           const parsedData = await SwaggerParser.validate(apiJson);
 
-          if (parsedData){
-            check = await parseAPIData(fileName , parsedData , apiJson); 
-            }
+          if (parsedData) {
+            check = await parseAPIData(fileName, parsedData, apiJson);
+          }
         } catch (e) {
           errorMessage(YAML_VALIDATOR, `File : ${file.name} : FAILED`);
           errorMsg(`Error: ${e?.message}`);
         }
-      }else{
+      } else {
         errorMessage(YAML_VALIDATOR, `Not a YAML Spec file : ${file.name}`);
       }
     });
   });
 
   return check;
- 
 };
 
-
-const parseAPIData = async (fileName , parsedData , apiJson) => {
+const parseAPIData = async (fileName, parsedData, apiJson) => {
   let check = false;
-  try{
+  try {
     for (const [path, obj] of Object.entries(apiJson.paths)) {
-
       for (const [reqType, api] of Object.entries(obj)) {
-
-        if (typeof api !== 'object' || api === null) {  continue; }
-          if (!api['x-proxy-name']) {
-            if (!api.hasOwnProperty('x-proxy-name')){
-              errorMessage(YAML_VALIDATOR, `File :${fileName} API-Path:${path} Error: Missing 'x-proxy-name'`);
-            }
-            check = false;
-            return;
+        if (typeof api !== "object" || api === null) {
+          continue;
+        }
+        if (!api["x-proxy-name"]) {
+          if (!api.hasOwnProperty("x-proxy-name")) {
+            errorMessage(
+              YAML_VALIDATOR,
+              `File :${fileName} API-Path:${path} Error: Missing 'x-proxy-name'`
+            );
           }
-          const version =  fileName.split('/')[1];
-          check = validateIndexBody(fileName, parsedData, apiJson, path, reqType, api, version);
+          check = false;
+          return;
+        }
+        const version = fileName.split("/")[1];
+        check = validateIndexBody(
+          fileName,
+          parsedData,
+          apiJson,
+          path,
+          reqType,
+          api,
+          version
+        );
       }
     }
-      if (check) {
+    if (check) {
       printMessage(`File: ${fileName} : PASSED`);
-      } else {
-        errorMsg(`Validation error in file ${fileName}`);
-      }
+    } else {
+      errorMsg(`Validation error in file ${fileName}`);
+    }
   } catch (e) {
     errorMsg(`Error: ${e?.message}`);
   }
   return check;
 };
 
-
-const validateIndexBody = (fileName , yamlData ,yamlJSONData ,path , reqType , api , version) =>{
-  try{
-    const pathJSON = yamlJSONData.paths[path][reqType]; 
+const validateIndexBody = (
+  fileName,
+  yamlData,
+  yamlJSONData,
+  path,
+  reqType,
+  api,
+  version
+) => {
+  try {
+    const pathJSON = yamlJSONData.paths[path][reqType];
     const converter = new showdown.Converter({
       ghCompatibleHeaderId: true,
       emoji: true,
@@ -94,54 +112,75 @@ const validateIndexBody = (fileName , yamlData ,yamlJSONData ,path , reqType , a
       extensions: [enrichHTMLFromMarkup(), showdownHighlight],
     });
 
-    const strRequestBody = pathJSON.requestBody ? JSON.stringify(pathJSON.requestBody, (key, value) => {
-      if (key === 'description' && typeof value === 'string') {
-        return converter.makeHtml(value);
-      } else{
-        return value;
-      }
-    }) : '';
+    const strRequestBody = pathJSON.requestBody
+      ? JSON.stringify(pathJSON.requestBody, (key, value) => {
+          if (key === "description" && typeof value === "string") {
+            return converter.makeHtml(value);
+          } else {
+            return value;
+          }
+        })
+      : "";
 
-    const strResponses = pathJSON.responses ? JSON.stringify(pathJSON.responses, (key, value) => {
-      if (key === 'description' && typeof value === 'string') {
-        return converter.makeHtml(value);
-      } else{
-        return value;
-      }
-    }) : '';
+    const strResponses = pathJSON.responses
+      ? JSON.stringify(pathJSON.responses, (key, value) => {
+          if (key === "description" && typeof value === "string") {
+            return converter.makeHtml(value);
+          } else {
+            return value;
+          }
+        })
+      : "";
 
-    const strParameters = pathJSON.parameters ? JSON.stringify(pathJSON.parameters) : '';
+    const strParameters = pathJSON.parameters
+      ? JSON.stringify(pathJSON.parameters)
+      : "";
 
     body = {
-      title: api['x-proxy-name'] ? api['x-proxy-name'] : (api.tags ? api.tags[0] : api.summary),
-      titleKW: api['x-proxy-name'] ? api['x-proxy-name'] : (api.tags ? api.tags[0] : api.summary), 
+      title: api["x-proxy-name"]
+        ? api["x-proxy-name"]
+        : api.tags
+        ? api.tags[0]
+        : api.summary,
+      titleKW: api["x-proxy-name"]
+        ? api["x-proxy-name"]
+        : api.tags
+        ? api.tags[0]
+        : api.summary,
       summary: api.summary,
       path,
-      description: api.description ? api.description : '',
+      description: api.description ? api.description : "",
       tags: api.tags,
       requestType: reqType,
       requestBody: strRequestBody,
-      xGroupName: api['x-group-name'] ? api['x-group-name'] : '',
-      xProxyName: api['x-proxy-name'] ? api['x-proxy-name'] : '',
-      xDisableDefaultExample: api['x-disable-default-example'] ? api['x-disable-default-example'] : false,
+      xGroupName: api["x-group-name"] ? api["x-group-name"] : "",
+      xProxyName: api["x-proxy-name"] ? api["x-proxy-name"] : "",
+      xDisableDefaultExample: api["x-disable-default-example"]
+        ? api["x-disable-default-example"]
+        : false,
       responses: strResponses,
       parameters: strParameters,
-      servers: yamlData.servers,   
+      servers: yamlData.servers,
       version: version,
-      xLinks: api['x-links'] ? api['x-links'] : {},
-      childProductName: Array.isArray(api['x-child-product-name']) ? api['x-child-product-name'] : [api['x-child-product-name']],     
-      xDefaultExample: api['x-default-example'] ? api['x-default-example'] : '',
-      xCore: api['x-core'] ? api['x-core'] : '',
-      xDefaultCore: api['x-core-default'] ? api['x-core-default'] : '',
-      xUseCases: api['x-use-cases'] ? api['x-use-cases'] : [],
-    };  
-  } catch(e) {
-    errorMessage(YAML_VALIDATOR ,`File :${fileName} with ${e?.message}`);
+      xLinks: api["x-links"] ? api["x-links"] : {},
+      childProductName: Array.isArray(api["x-child-product-name"])
+        ? api["x-child-product-name"]
+        : [api["x-child-product-name"]],
+      xDefaultExample: api["x-default-example"] ? api["x-default-example"] : "",
+      xCore: api["x-core"] ? api["x-core"] : "",
+      xDefaultCore: api["x-core-default"] ? api["x-core-default"] : "",
+      xUseCases: api["x-use-cases"] ? api["x-use-cases"] : [],
+    };
+  } catch (e) {
+    errorMessage(YAML_VALIDATOR, `File :${fileName} with ${e?.message}`);
     return false;
   }
 
   if (body.xProxyName.length > 0 && /^[^A-Za-z]|[\W]$/.test(body.xProxyName)) {
-    errorMessage(YAML_VALIDATOR, `File :${fileName} API-Path:${path} Error: Empty space at start/end of 'x-proxy-name'`);
+    errorMessage(
+      YAML_VALIDATOR,
+      `File :${fileName} API-Path:${path} Error: Empty space at start/end of 'x-proxy-name'`
+    );
     return false;
   }
   return true;
@@ -149,45 +188,43 @@ const validateIndexBody = (fileName , yamlData ,yamlJSONData ,path , reqType , a
 
 const hasAPIs = async (dir) => {
   const files = await fs.promises.readdir(dir, { withFileTypes: true });
-  let check = false; 
-  for (const file of files) {   
-    if (file?.name === 'tenant.json'){
-      try{ 
-       
+  let check = false;
+  for (const file of files) {
+    if (file?.name === "tenant.json") {
+      try {
         const fileName = `${dir}/${file.name}`;
-        const content = await fs.promises.readFile(fileName, 'utf8'); 
-        const tenantData = JSON.parse(content);   
-        if (tenantData?.apiVersions && tenantData?.apiVersions.length > 0){
+        const content = await fs.promises.readFile(fileName, "utf8");
+        const tenantData = JSON.parse(content);
+        if (tenantData?.apiVersions && tenantData?.apiVersions.length > 0) {
           return true;
-        } 
-      }catch (e){
-        errorMessage(YAML_VALIDATOR  ,e?.message);
+        }
+      } catch (e) {
+        errorMessage(YAML_VALIDATOR, e?.message);
         check = false;
-      }  
-    }   
-  };
+      }
+    }
+  }
   return check;
 };
 
-
-const main = async() => {
+const main = async () => {
   try {
-    if ( args?.length > 0){  
+    if (args?.length > 0) {
       // Check for API version in tenant configuration file
-      const checkAPIs = await hasAPIs(folder+"/config");
-      if (checkAPIs){
+      const checkAPIs = await hasAPIs(folder + "/config");
+      if (checkAPIs) {
         const refFolder = provideReferenceFolder(folder);
         await validateDir(refFolder);
-      } else{
-        printMessage('SKIPPED');
+      } else {
+        printMessage("SKIPPED");
       }
-   }else{
-    errorMessage(YAML_VALIDATOR, 'No Path for reference dir. defined');
-   }
+    } else {
+      errorMessage(YAML_VALIDATOR, "No Path for reference directory defined.");
+    }
   } catch (e) {
     errorMessage(YAML_VALIDATOR, e?.message);
   }
-}
+};
 
 if (require.main === module) {
   main();
