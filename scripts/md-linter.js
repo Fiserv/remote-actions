27c6631f18b7072aca49a4c3a47d4899d8f2babe
Minh Pham <minh.pham1@fiserv.com>
2024-08-12 16:51:38 -0700

    Test new options

--- scripts/md-linter.js
+++ scripts/md-linter.js
@@ -3,55 +3,7 @@ const showdown = require("showdown");
 const fs = require("fs");
 const args = process.argv.slice(2);
 const folder = args?.[0] + "/docs";
-const {
-  enrichHTMLFromMarkup,
-  showdownHighlight,
-  mdExtension,
-} = require("./utils/md-utils");
-const { errorMessage, errorMsg, printMessage, warningMsg } = require("./utils/tools");
-let urlsArr = [];
-
-const converter = new showdown.Converter({
-  ghCompatibleHeaderId: true,
-  emoji: true,
-  disableForced4SpacesIndentedSublists: true,
-  literalMidWordUnderscores: true,
-  tables: true,
-  extensions: [enrichHTMLFromMarkup(), showdownHighlight, mdExtension],
-});
-
-converter.addExtension(() => {
-  return [
-    {
-      type: "output",
-      regex: /<a\shref[^>]+>/g,
-      replace: function (text) {
-        const url = text.match(/"(.*?)"/)[1];
-        if (url.startsWith("http:") || url.startsWith("https:")) {
-          urlsArr.push(url);
-          return '<a href="' + url + '" target="_blank">';
-        }
-        return text;
-      },
-    }
-  ];
-}, "externalLink");
-
-converter.addExtension(() => {
-  return [
-  {
-    type: 'output',
-    filter: function (htmlContent) {
-        const imgRegex = /<img.*?src=["'](.*?)["']/g;
-        let match;
-        while ((match = imgRegex.exec(htmlContent)) !== null) {
-          urlsArr.push(match[1]);
-        }
-        return htmlContent;
-    }
-  }
-  ]
-}, "extractImageUrls");
+const { errorMessage, errorMsg, printMessage } = require("./utils/tools");
 
 const markdownlinter = async (dir) => {
   fs.readdir(dir, { withFileTypes: true }, (err, files) => {
@@ -75,7 +27,6 @@ const markdownlinter = async (dir) => {
               "heading-style": false
             },
           };
-          // const result = markdownlint.sync(options);
           markdownlint(options, function callback(err, result) {
             if (!err) {
               if (result.toString().length > 0) {
@@ -83,7 +34,7 @@ const markdownlinter = async (dir) => {
                   "MD LINTER",
                   `PLEASE CHECK FOLLOWING LINTER ISSUES WITHIN THE FILE : ${fileName.split('/docs/')[1]}`
                 );
-                warningMsg(result);
+                errorMsg(result);
               } else {
                 printMessage(`${fileName.split('/docs/')[1]} - LINTER PASSED`);
               }
